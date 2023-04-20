1379e9f384f8258bdad6312b8cbb14274a510e09
LuongThu186 <thula20406@st.uel.edu.vn>
2023-04-20 17:10:17 +0700

    update edit infor profile

--- my-project/src/app/profile/profile.component.ts
+++ my-project/src/app/profile/profile.component.ts
@@ -1,4 +1,5 @@
-import { Component, Input } from '@angular/core';
+import { ViewChild } from "@angular/core";
+import { Component, ElementRef } from '@angular/core';
 
 @Component({
   selector: 'app-profile',
@@ -20,5 +21,60 @@ export class ProfileComponent {
   tabStatus(sts:any){
     this.st=sts;
     console.log(this.st)
+  }
+
+  
+  name = 'Nhà thuốc Benzen';
+  gender = 'Nữ';
+  phone = '0332583xxx'
+  birthday = '03/04/2002';
+  email = 'benzen@gmail.com';
+  avatarUrl = 'assets/image/profile/avt.png';
+
+
+  nameEdit: string='';
+  genderEdit = '';
+  phoneEdit='';
+  birthdayEdit = '';
+  emailEdit: string='';
+
+  editing =false;
+
+  edit() {
+    this.nameEdit = this.name;
+    this.genderEdit = this.gender;
+    this.phoneEdit=this.phone;
+    this.birthdayEdit = this.birthday;
+    this.emailEdit = this.email;
+    this.editing = true;
+  }
+
+  saveInfor() {
+    this.name = this.nameEdit;
+    this.gender = this.genderEdit;
+    this.phone=this.phoneEdit;
+    this.birthday = this.birthdayEdit;
+    this.email = this.emailEdit;
+    this.editing = false;
+  }
+
+  onFileSelected(event: any) {
+    const file: File = event.target.files[0];
+    const reader = new FileReader();
+    reader.readAsDataURL(file);
+    reader.onload = () => {
+      this.avatarUrl = reader.result as string;
+    };
+  }
+
+  // src="" 
+  cancelEdit() {
+    this.name = this.name;
+    this.gender = this.gender;
+    this.phone=this.phone;
+    this.birthday = this.birthday;
+    this.email = this.email;
+    this.editing = false;
+    this.editing = false;
   }
 }
