df69de74655a0744e771dfd1e67dc8d4c8dd74ac
LuongThu186 <thula20406@st.uel.edu.vn>
2023-04-23 12:31:49 +0700

    category-admin

--- my-admin/src/app/app-routing.module.ts
+++ my-admin/src/app/app-routing.module.ts
@@ -1,7 +1,22 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AdminCaterogyManagementComponent } from './admin-caterogy-management/admin-caterogy-management.component';
+import { AdminCustomerDetailManagementComponent } from './admin-customer/admin-customer-detail-management/admin-customer-detail-management.component';
+import { AdminCustonerManagementComponent } from './admin-customer/admin-custoner-management/admin-custoner-management.component';
+import { AdminMessengerComponent } from './admin-messenger/admin-messenger.component';
+import { AdminOrderDetailManagementComponent } from './admin-order/admin-order-detail-management/admin-order-detail-management.component';
+import { AdminOrderManagementComponent } from './admin-order/admin-order-management/admin-order-management.component';
+import { AdminUncompleteOrderManagementComponent } from './admin-order/admin-uncomplete-order-management/admin-uncomplete-order-management.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:"admin-order-management", component: AdminOrderManagementComponent },
+  {path:"admin-order-detail-management", component: AdminOrderDetailManagementComponent},
+  {path:"admin-uncomplete-order-management", component: AdminUncompleteOrderManagementComponent},
+  {path:"admin-custoner-management", component: AdminCustonerManagementComponent},
+  {path:"admin-customer-detail-management", component: AdminCustomerDetailManagementComponent},
+  {path:"admin-messenger", component: AdminMessengerComponent},
+  {path:"admin-caterogy-management", component: AdminCaterogyManagementComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
